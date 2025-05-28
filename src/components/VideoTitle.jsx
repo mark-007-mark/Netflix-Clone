96bd4003104886d7bb7d81b3540cab1e5a5b0ccf
Vicky Singh <vicky1910098@akgec.ac.in>
2025-05-28 22:14:56 +0530

    background video css done

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-52 px-12 absolute text-white">
-      <h1 className="text-6xl font-bold w-2/6">{title}</h1>
-      <p className="py-6 text-lg w-2/5">{overview}</p>
-      <div className="flex gap-3">
-        <button className="bg-gray-500 text-white py-4 px-12 text-xl bg-opacity-50 rounded-lg hover:bg-opacity-80 flex items-center">
-          ▶ Play
-        </button>
-        <button className="mx-2 bg-gray-500 text-white py-4 px-12 text-xl bg-opacity-50 rounded-lg hover:bg-opacity-40 flex items-center">
-          ℹ More Info
-        </button>
+    <div className="w-screen aspect-video absolute top-0 px-24 bg-gradient-to-r from-black">
+      <div className="mt-[20%] w-full">
+        <h1 className="text-7xl font-bold text-white w-1/4 max-w-2xl">{title}</h1>
+        <p className="text-white text-lg max-w-xl py-6 line-clamp-3">{overview}</p>
+        <div className="flex gap-4">
+          <button className="bg-white text-black py-3 px-8 text-xl rounded-lg hover:bg-opacity-80 transition flex items-center gap-2">
+            <span>▶</span> Play
+          </button>
+          <button className="bg-gray-500 bg-opacity-50 text-white py-3 px-8 text-xl rounded-lg hover:bg-opacity-70 transition flex items-center gap-2">
+            <span>ℹ</span> More Info
+          </button>
+        </div>
       </div>
     </div>
   );
