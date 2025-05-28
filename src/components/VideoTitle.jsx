899e48d8a22d2b26c87ca8590ecc81efd4a8398e
Vicky Singh <vicky1910098@akgec.ac.in>
2025-05-29 01:16:13 +0530

    css fix

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const VideoTitle = ({ title, overview }) => {
   return (
     <div className="w-screen aspect-video absolute top-0 px-24 bg-gradient-to-r from-black">
-      <div className="mt-[20%] w-full">
-        <h1 className="text-7xl font-bold text-white w-1/4 max-w-2xl">{title}</h1>
-        <p className="text-white text-lg max-w-xl py-6 line-clamp-3">{overview}</p>
+      <div className="mt-[26%] w-full">
+        <h1 className="text-5xl font-bold text-white w-1/4 max-w-2xl">{title}</h1>
+        <p className="text-white text-lg max-w-96 py-6 line-clamp-3">{overview}</p>
         <div className="flex gap-4">
           <button className="bg-white text-black py-3 px-8 text-xl rounded-lg hover:bg-opacity-80 transition flex items-center gap-2">
             <span>▶</span> Play
